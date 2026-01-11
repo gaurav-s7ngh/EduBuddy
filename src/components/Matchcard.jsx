2c1f6fc9d79ff6873bd82371761bf43480c37012
gaurav-s7ngh <gauravsinghguru07@gmail.com>
2026-01-12 00:23:17 +0530

    new

--- src/components/Matchcard.jsx
+++ src/components/Matchcard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useMemo } from 'react';
 import '../styles/Dashboard.css'; 
-import '../styles/Matchcard.css'; 
+import '../styles/MatchCard.css'; 
 import avatar from '/avatar.png';
 // --- NEW: Import definitions and your profile hook ---
 
